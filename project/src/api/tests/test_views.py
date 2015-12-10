ade5e4d1792182fd67e48558e93ab2eb8958c9b6
Mariana Bedran Lesche <maribedran@gmail.com>
2015-12-10 21:16:43 -0200

    Tests list view response

--- project/src/api/tests/test_views.py
+++ project/src/api/tests/test_views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import json
 from django.test import TestCase
 from model_mommy import mommy
 
@@ -12,7 +13,25 @@ class ListProductsViewTest(TestCase):
 
     def setUp(self):
         self.url = reverse('api:list_products')
+        self.product1 = mommy.make(Product)
+        self.product2 = mommy.make(Product)
 
     def test_response_returns_status_code_200(self):
         response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
+
+    def test_response_has_coorect_data(self):
+        response = self.client.get(self.url)
+        expected_data = [
+            {
+                'id': self.product1.id,
+                'name': self.product1.name,
+                'priority': self.product1.priority,
+            },
+            {
+                'id': self.product2.id,
+                'name': self.product2.name,
+                'priority': self.product2.priority,
+            },
+        ]
+        self.assertEqual(expected_data, json.loads(response.content))
