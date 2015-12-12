47f6a73656e8075bbe4a9058f7677846e8c9c8fb
Mariana Bedran Lesche <maribedran@gmail.com>
2015-12-12 17:35:36 -0200

    Creates dummy page to show all products using angular template

--- project/src/urls.py
+++ project/src/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('src.web_app.urls', namespace='web_app')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('src.api.urls', namespace='api')),
 ]
